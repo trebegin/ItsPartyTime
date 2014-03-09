2f3b12b35f99f8d7a0f25d487458cd29210537d6
Becky <Becky@Becky-PC>
2014-03-09 15:33:17 -0600

    Changed project target

--- ItsPartyTime/src/com/example/itspartytime/JoinPartyPage.java
+++
@@ -1,5 +0,0 @@
-package com.example.itspartytime;
-
-public class JoinPartyPage {
-
-}
--- ItsPartyTime/src/com/itspartytime/MainActivity.java
+++ ItsPartyTime/src/com/itspartytime/MainActivity.java
@@ -1,10 +1,10 @@
-package com.example.itspartytime;
+package com.itspartytime;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class Party extends Activity {
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
