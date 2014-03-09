34564b4d01e303370165fc12fc3fc4b8af9a6b4f
Becky <Becky@Becky-PC>
2014-03-09 16:14:11 -0600

    Skeleton of Playlist

--- ItsPartyTime/src/com/itspartytime/GoogleMusicInterface.java
+++ ItsPartyTime/src/com/itspartytime/GoogleMusicInterface.java
@@ -1,5 +1,17 @@
 package com.itspartytime;
 
 public class GoogleMusicInterface {
+	
+	void playSong(int songID){
+		
+	}
+	
+	void pause() {
+		
+	}
+	
+	void reorderPlaylist(Playlist playlist){
+		
+	}
 
 }
--- ItsPartyTime/src/com/itspartytime/Playlist.java
+++ ItsPartyTime/src/com/itspartytime/Playlist.java
@@ -1,5 +1,53 @@
 package com.itspartytime;
 
-public class Playlist {
+import java.util.ArrayList;
+
+public class Playlist 
+{
+	
+	private ArrayList<Song> songList; // probably want queue instead, but not sure which kind
+	private Song currentSong;
+		
+	/**
+	 * Changes the vote counts in 'song' depending on the value of vote: 
+	 * 1 is upvote, 2 is downvote
+	 * 
+	 * @param vote
+	 * @param song
+	 */
+	public void vote(int vote, Song song)
+	{
+		
+	}
+	
+	/**
+	 * changes songList order, assumes 'song' was just upvoted or downvoted 
+	 * calls GoogleMusicInterface
+	 * 
+	 * @param song
+	 */
+	private void updatePlaylist(Song song)
+	{
+		
+	}
+	
+	/**
+	 * pauses/plays current song depending, calls GoogleMusicInterface
+	 */
+	public void pause()
+	{
+		
+	}
+	
+	/**
+	 * changes the current song to 'song', calls GoogleMusicInterface
+	 * 
+	 * @param song
+	 */
+	public void changeSong(Song song)
+	{
+		
+	}
+
 
 }
