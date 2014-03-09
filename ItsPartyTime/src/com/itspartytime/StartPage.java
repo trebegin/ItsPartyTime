146ce4d50dd2db0c9ff01dc08d78be026c1006b0
trebegin <trebegin@gmail.com>
2014-03-09 16:19:29 -0600

    added comments yo

--- ItsPartyTime/src/com/itspartytime/StartPage.java
+++ ItsPartyTime/src/com/itspartytime/StartPage.java
@@ -3,11 +3,17 @@ package com.itspartytime;
 public class StartPage 
 {
 	
+	/**
+	 * Creates and moves to CreatePartyPage View
+	 */
 	public void createPartyPage()
 	{
 		
 	}
 	
+	/**
+	 * Creates and moves to JoinPartyPage View
+	 */
 	public void joinPartyPage()
 	{
 		
