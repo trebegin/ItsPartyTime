2619d1aafb1c6246760c6963c56ee93d08bc6acc
trebegin <trebegin@gmail.com>
2014-03-09 16:38:46 -0600

    Created methods for JoinPartyPage

--- ItsPartyTime/src/com/itspartytime/JoinPartyPage.java
+++ ItsPartyTime/src/com/itspartytime/JoinPartyPage.java
@@ -2,5 +2,21 @@ package com.itspartytime;
 
 public class JoinPartyPage 
 {
+	/**
+	 * Creates and moves to PlaylistViewPage View
+	 */
+	public void createPlaylistView()
+	{
+		
+	}
+	
+	/**
+	 * Connects to the host device to receive party information 
+	 * @param partyName
+	 */
+	public void connectToParty(String partyName)
+	{
+		
+	}
 
 }
